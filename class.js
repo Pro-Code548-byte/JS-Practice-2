684432f2597efab0dfb758ec69257783dcecf37e
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-27 08:40:42 +0100

    Adding the seventh command

---
+++ class.js
@@ -0,0 +1,9 @@
+function printClassList() {
+  let classlist = ["Amara", "Dele", "Chinwe", "Tunde", "Femi", "Nneka"];
+
+  for (let i = 0; i < classlist.length; i++) {
+    console.log("Student " + (i + 1) + ": " + classlist[i]);
+  }
+}
+
+printClassList();
