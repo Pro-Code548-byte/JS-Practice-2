591d1860f730eb030378f3c1bf3ea232e1780aba
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-27 08:58:22 +0100

    Corrections

--- cart.js
+++ cart.js
@@ -1,10 +1,8 @@
 function updateCart() {
   let cart = ["shirt", "shoes", "hat"];
 
-  // Add "belt" and "watch" to the cart
   cart.push("belt", "watch");
 
-  // Print the total number of items
   console.log(cart.length);
 }
 
--- rain.js
+++ rain.js
@@ -2,12 +2,10 @@ function averageRainfall() {
   let rainfall = [120, 85, 200, 60, 175, 140];
   let total = 0;
 
-  // Add all values
   for (let i = 0; i < rainfall.length; i++) {
     total += rainfall[i];
   }
 
-  // Calculate average
   let average = total / rainfall.length;
 
   console.log(average);
