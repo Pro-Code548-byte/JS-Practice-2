9e1eecef1e1cfc163ec849de1991b4fe03ab7184
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-27 08:55:45 +0100

    Adding the fourteenth command

---
+++ cart.js
@@ -0,0 +1,11 @@
+function updateCart() {
+  let cart = ["shirt", "shoes", "hat"];
+
+  // Add "belt" and "watch" to the cart
+  cart.push("belt", "watch");
+
+  // Print the total number of items
+  console.log(cart.length);
+}
+
+updateCart();
