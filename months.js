1ddb2bd3992f8f3af7e8203f25878db825dc0047
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-27 08:01:44 +0100

    Adding the first command

---
+++ months.js
@@ -0,0 +1,8 @@
+function printMonths() {
+  let months = ["January", "February", "March", "April", "May"];
+
+  console.log(months[1]); // second month
+  console.log(months[3]); // fourth month
+}
+
+printMonths();
